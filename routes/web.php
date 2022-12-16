e7ad3fbc77dda8ed4fd83fe253f430bd5f4c31db
JaquelineBarragan <jaqbarraganpaypal@gmail.com>
2022-12-16 11:38:39 -0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/seed', function () {
+    Artisan::call('db:seed');
+});
+Route::get('/', function () {
+    return redirect('/login');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::view('/{a}', 'home');
+Route::view('/{a}/{b}', 'home');
+Route::view('/{a}/{b}/{c}', 'home');
+Route::view('/{a}/{b}/{c}/{d}', 'home');
+Route::view('/{a}/{b}/{c}/{d}/{e}', 'home');
